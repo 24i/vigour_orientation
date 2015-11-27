f57187da5e165ac190e7083dc2464586486a7e59
Valerio Barrila <valerio.barrila@gmail.com>
2015-11-27 12:48:32 +0100

    using real bridge for data flow tests

--- test/bridge/index.js
+++ test/bridge/index.js
@@ -2,7 +2,7 @@
 // tests data flows between native and js side using the bridge (if native) or the mock (if browser)
 describe('Testing data flows between native and js using the bridge', () => {
   require('../../lib/native')
-  var bridge = global.vigour.native.bridge /* require('../mockBridge') */
+  var bridge = window.vigour.native.bridge /* require('../mockBridge') */
 
   // bridge.send(pluginId, fnName, opts, cb)
   // bridge.ready(err, response, pluginId)

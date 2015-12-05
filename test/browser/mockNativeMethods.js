4e0d0c208ac8dc952c22dcfc44cafb1a5b43b866
ninjatux <valerio.barrila@gmail.com>
2015-12-05 11:39:50 +0100

    added tests

--- test/browser/mockNativeMethods.js
+++ test/browser/mockNativeMethods.js
@@ -4,50 +4,22 @@ var bridge = require('vigour-wrapper/lib/bridge')
 var mock = {
   label: 'bridge',
   init (opts, cb) {
-    console.log('bridge init')
     setTimeout(function () {
       cb && cb(null)
     })
   },
   orientation (opts, cb) {
-    console.log('bridge orientation')
     setTimeout(function () {
       console.log(cb)
       cb && cb(null)
     })
   },
   locked (opts, cb) {
-    console.log('bridge lock')
-    console.log('opts', opts, 'cb', cb)
     // opts: true > lock, false > unlock
     setTimeout(function () {
       console.log('calling callbackst!')
       cb && cb(null)
     })
-  },
-  // lock (opts, cb) {
-  //   console.log('bridge lock')
-  //   console.log('opts', opts, 'cb', cb)
-  //   setTimeout(function () {
-  //     cb && cb(null)
-  //   })
-  // },
-  // unlock (opts, cb) {
-  //   console.log('bridge unlock')
-  //   console.log('opts', opts, 'cb', cb)
-  //   setTimeout(function () {
-  //     cb && cb(null)
-  //   })
-  // },
-  events: {
-    changeToPortrait: {
-      eventType: 'orientation',
-      data: 'portrait'
-    },
-    changeToLandscape: {
-      eventType: 'orientation',
-      data: 'landscape'
-    }
   }
 }
 
--- test/browser/mockPlatform.js
+++ test/browser/mockPlatform.js
@@ -3,7 +3,6 @@ exports.platform = {
   on: {
     init: {
       orientation () {
-        console.log('mock init')
         setTimeout(() => {
           this.parent.ready.val = true
         })
@@ -11,7 +10,6 @@ exports.platform = {
     },
     orientation: {
       orientation (data) {
-        console.log('mock orientation')
         this.parent.locked.val = true
         data.done(null)
         return
@@ -19,7 +17,6 @@ exports.platform = {
     },
     locked: {
       orientation (obj) {
-        console.log('mock lock')
         obj.done(null)
         // i'm going to lock for real in native
         return

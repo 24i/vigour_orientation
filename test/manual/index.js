fec826720a8af77e2a4e95b8c7ef932b36df0376
ninjatux <valerio.barrila@gmail.com>
2015-12-08 13:58:58 +0100

    added web implementation

--- test/manual/index.js
+++ test/manual/index.js
@@ -1,9 +1,10 @@
 'use strict'
 require('gaston-tester')
+var agent = require('vigour-ua')(navigator.userAgent)
 
 var tests = require('../tests')
 describe('Orientation', function () {
-  this.timeout(25000)
-  tests()
+  // this.timeout(25000)
+  tests(null, agent.browser ? 'browser' : null)
 })
 
--- test/tests.js
+++ test/tests.js
@@ -2,74 +2,86 @@
 
 module.exports = function (inject, type) {
   var or
-  var manual = !inject
+  var manual = !inject && type !== 'browser'
+  var web = !inject && type === 'browser'
 
   it('require env', function () {
+    console.warn('--- [0][TEST] require plugin')
     or = require('../lib')
   })
 
   if (inject) {
     it('create instance with mock properties', function () {
+      console.warn('--- [1][TEST] require inject')
       or = new or.Constructor(inject)
     })
   }
 
   it('should be ready after the init', function (done) {
+    console.warn('--- [2][TEST] wating for ready')
     or.ready.is(true, () => {
+      console.warn('--- [2][TEST] ready received')
       done()
     })
   })
 
-  it('should change the orientation to landscape and lock it', function (done) {
-    or.locked.val = 0
-    var itfired = false
-    or.on('data', function () {
-      expect(or.locked.val).to.be.true
-      itfired = true
-      if (!manual) {
-        done()
+  if (!web) {
+    it('should change the orientation to landscape and lock it', function (done) {
+      this.timeout(25000)
+      alert('put device orientation on portrait if not already')
+      or.locked.val = 0
+      var itfired = false
+      or.on('data', function () {
+        itfired = true
+        if (!manual) {
+          expect(or.locked.val).to.be.true
+          done()
+        }
+      })
+      or.val = 'landscape'
+
+      if (manual) {
+        setTimeout(() => {
+          expect(itfired).to.be.true
+          alert('the orientation should now be landscape, please check')
+        }, 2000)
       }
     })
 
-    or.val = 'landscape'
+    it('should change the orientation to portrait and lock it', function (done) {
+      this.timeout(25000)
+      or.locked.val = 0
+      var itfired = false
+      or.on('data', function () {
+        expect(or.locked.val).to.be.true
+        itfired = true
+        if (!manual) {
+          done()
+        }
+      })
 
-    if (manual) {
-      setTimeout(() => {
-        expect(itfired).to.be.true
-        alert('the orientation should now (already) be landscape')
-      }, 2000)
-    }
-  })
+      or.val = 'portrait'
 
-  it('should change the orientation to portrait and lock it', function (done) {
-    or.locked.val = 0
-    var itfired = false
-    or.on('data', function () {
-      expect(or.locked.val).to.be.true
-      itfired = true
-      if (!manual) {
-        done()
+      if (manual) {
+        setTimeout(() => {
+          expect(itfired).to.be.true
+          alert('the orientation should now (already) be portrait')
+        }, 2000)
       }
-    })
-
-    or.val = 'portrait'
-
-    if (manual) {
-      setTimeout(() => {
-        expect(itfired).to.be.true
-        alert('the orientation should now (already) be portrait')
-      }, 2000)
-    }
-  })
-
-  if (manual) {
-    it('should be locked', function (done) {
-      alert('orientation should be locked, you now have 5 seconds to confirm...')
-      setTimeout(function () {
-        done()
-      }, 5000)
     })
   }
+
+  it('should inform on orientation chnages', function (done) {
+    this.timeout(25000)
+    alert('try to change device orientation 2 times')
+    var portrait = false
+    var landscape = false
+    or.on('data', function (data) {
+      if (data === 'portrait') portrait = true
+      if (data === 'landscape') landscape = true
+      if (portrait && landscape) done()
+    })
+  })
 
   if (manual) {
     it('should be locked', function (done) {

9c3f9baa1937cc0dd4777ccdb9cfd5f369e4eabd
ninjatux <valerio.barrila@gmail.com>
2015-12-14 14:56:02 +0100

    removed logs, more tests for devices

--- test/tests.js
+++ test/tests.js
@@ -2,42 +2,46 @@
 
 module.exports = function (inject, type) {
   var or
-  var manual = !inject && type !== 'browser'
   var web = !inject && type === 'browser'
+  var bridge = type && type.label === 'mockBridge'
+  var manual = !inject && !bridge && !web
 
   it('require', function () {
-    console.warn('--- [0][TEST] require plugin')
     or = require('../lib')
   })
 
   if (inject) {
     it('create instance with mock properties', function () {
-      console.warn('--- [1][TEST] require inject')
       or = new or.Constructor(inject)
     })
   }
 
   it('should be ready after the init', function (done) {
-    console.warn('--- [2][TEST] wating for ready')
     or.ready.is(true, () => {
-      console.warn('--- [2][TEST] ready received')
       done()
     })
   })
 
   it('should inform on orientation chnages', function (done) {
     this.timeout(25000)
-    alert('try to change device orientation 2 times')
     var portrait = false
     var landscape = false
     or.on('data', function (data) {
       if (data === 'portrait') portrait = true
       if (data === 'landscape') landscape = true
       if (portrait && landscape) done()
-    })
+    }, 'changes')
+    if (manual) {
+      alert('try to change device orientation 2 times')
+    } else if (!web) {
+      or.val = or.val === 'portrait' ? 'landscape' : 'portrait'
+      setTimeout(function () {
+        or.val = or.val === 'portrait' ? 'landscape' : 'portrait'
+      }, 300)
+    }
   })
 
-  if (!web) {
+  if (!web || bridge) {
     it('should change the orientation to portrait and lock it', function (done) {
       var timeout
       if (manual) {
